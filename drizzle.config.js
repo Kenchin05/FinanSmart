70c933d9080f584aacf597849149644e598caa1f
Kenchin05 <rairishav2002@gmail.com>
2024-11-09 00:14:25 +0530

    FinanSmart going live

---
+++ drizzle.config.js
@@ -0,0 +1,10 @@
+export default {
+  dialect: "postgresql",
+  schema: "./utils/schema.jsx",
+  out: "./drizzle",
+  dbCredentials: {
+    url: "postgresql://neondb_owner:5IvXcsdZjLY0@ep-patient-brook-a5utmkem.us-east-2.aws.neon.tech/neondb?sslmode=require",
+    connectionString:
+      "postgresql://neondb_owner:5IvXcsdZjLY0@ep-patient-brook-a5utmkem.us-east-2.aws.neon.tech/neondb?sslmode=require",
+  },
+};
